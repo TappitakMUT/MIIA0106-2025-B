af042d0c443a8b9308ca306c28d37027f98d6769
TappitakMUT <6812100019@mut.ac.th>
2026-01-04 17:09:49 +0700

    LAB08

---
+++ LAB02/LAB2/LAB2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{ 
+	string itemName;
+	float itemPrice;
+	int itemQuantity;
+	float total;
+
+	cout << "Enter Item Name: ";
+	getline(cin, itemName);
+
+	cout << "Enter itemPrice: ";
+	cin >> itemPrice;
+
+	cout << "Enter item Quantity: ";
+	cin >> itemQuantity;
+
+	total = itemPrice * itemQuantity;
+	cout << "---Invoice---" << endl;
+	cout << "item:"<< itemName<<endl;
+	cout <<"Price: $"<<itemPrice<< endl;
+	cout <<"Quantity : "<<itemQuantity<< endl;
+	cout << "total: $"<<total<<endl;
+
+	return 0;
+	
+}
---
+++ LAB08/LAB0803/LAB0803.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    // TODO
+    string id;
+    string nickname;
+    string lineID;
+    string phone;
+};
+
+// TODO: เขียนฟังก์ชันนี้
+void printStudent(Student s1) {
+    // TODO
+    cout << "ID\t nickname\t Line ID\t Phone\n";
+    cout << s1.id << "\t" << s1.nickname << "\t" << s1.lineID << "\t" << s1.phone << "\n";
+}
+void inputStudent(Student& s1) {
+    cout << "Enter ID: ";
+    cin >> s1.id;
+    cout << "Enter Nickname: ";
+    cin >> s1.nickname;	
+    cout << "Enter Line ID: ";
+    cin >> s1.lineID;
+    cout << "Enter Phone: ";
+	cin >> s1.phone;
+}
+int main() {
+    Student s1;
+
+    cout << "=== Input Student 1 ===\n";
+    // TODO: cin >> ...
+	inputStudent(s1);
+
+    cout << "\n=== Output (from function) ===\n";
+    // TODO: เรียก printStudent
+	printStudent(s1);
+
+    return 0;
+}
+
